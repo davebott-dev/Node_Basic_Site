45ea9885995e48b0a685b9bbf841dbe450c79aae
= <bottenbergdave@gmail.com>
2024-08-24 09:49:29 -0400

    added error path

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
+//import required to create a server
 var http = require('http');
 var url = require('url');
+//import required to read html files and render them on screen
 var fs = require('fs');
 
 
 http.createServer( (req, res) => {
-
+//check what the request url path is and display the correct content
   if(req.url == '/' || req.url == '/index') {
-    
     fs.readFile('index.html', (err,data)=> {
+      //display an error page if path is bad
+      if(err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('404 Not Found.')
+      }
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
     });
 
   } else if( req.url == '/about') {
-    
     fs.readFile('about.html', (err,data)=> {
+      if(err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('404 Not Found.')
+      }
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
     });
 
   } else if (req.url == '/contact-me') {
-    
     fs.readFile('contact-me.html', (err,data)=> {
+      if(err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('404 Not Found.')
+      }
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
     });
 
   } else {
+    //display an error page if try to go to a path other than the above paths
       fs.readFile('404.html', (err,data)=> {
+        if(err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.end('404 Not Found.')
+        }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
@@ -38,7 +55,7 @@ http.createServer( (req, res) => {
   
     }  
 }).listen(8080, ()=> {
-  console.log('server has started')
+  console.log('The server has started')
 });
 
 
