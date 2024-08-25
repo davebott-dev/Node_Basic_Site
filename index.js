e9ca4d6b81b19f4089f07d44be187fbf5b693fe9
= <bottenbergdave@gmail.com>
2024-08-25 08:46:15 -0400

    updated file using express to handle serving static html

--- index.js
+++ index.js
@@ -1,61 +1,28 @@
-//import required to create a server
-var http = require('http');
-var url = require('url');
-//import required to read html files and render them on screen
-var fs = require('fs');
+//imported express to handle static html pages easier
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = express.Router();
+const PORT = 3000;
 
-
-http.createServer( (req, res) => {
-//check what the request url path is and display the correct content
-  if(req.url == '/' || req.url == '/index') {
-    fs.readFile('index.html', (err,data)=> {
-      //display an error page if path is bad
-      if(err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('404 Not Found.')
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-
-  } else if( req.url == '/about') {
-    fs.readFile('about.html', (err,data)=> {
-      if(err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('404 Not Found.')
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-
-  } else if (req.url == '/contact-me') {
-    fs.readFile('contact-me.html', (err,data)=> {
-      if(err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('404 Not Found.')
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-
-  } else {
-    //display an error page if try to go to a path other than the above paths
-      fs.readFile('404.html', (err,data)=> {
-        if(err) {
-          res.writeHead(404, {'Content-Type': 'text/html'});
-          res.end('404 Not Found.')
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-      });
-  
-    }  
-}).listen(8080, ()=> {
-  console.log('The server has started')
+//added router to handle the site routes
+router.get('/',(req,res)=> {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+router.get('/about',(req,res)=> {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+router.get('/contact-me',(req,res)=> {
+  res.sendFile(path.join(__dirname, 'contact-me.html'));
+});
+router.get('/404',(req,res)=> {
+  res.sendFile(path.join(__dirname, '404.html'));
+});
+
+app.use('/', router);
+app.listen(PORT, ()=> console.log(`Listening on PORT ${PORT}`))
+
+
+
 
 
